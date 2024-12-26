daa586f19412e2fb279edd7555c5bdce882423ab
Jacek Skrzypek <devontheroof@gmail.com>
2024-12-26 20:09:24 +0100

    testing DB WIP

--- src/db/db.spec.ts
+++ src/db/db.spec.ts
@@ -4,6 +4,10 @@ import { Fragment } from './entities/Fragment';
 import { env } from '../env';
 import { DataSource } from 'typeorm';
 import uuid from 'uuid4';
+import { Person } from './entities/Person';
+import { Country } from './entities/Country';
+import { Bio } from './entities/Bio';
+import { Tag } from './entities/Tag';
 
 describe('Example Test', () => {
   it('should pass', () => {
@@ -28,23 +32,45 @@ describe('Database', () => {
 
   it('should insert and retrive test data', async () => {
     const connection = getDbConnection();
-    const fragmentRepository = connection.getRepository(Fragment);
 
+    const fragmentRepository = connection.getRepository(Fragment);
     const testFragment = new Fragment();
     testFragment.fragment_id = uuid();
     testFragment.title = 'Test Fragment';
     testFragment.length = 120;
     testFragment.player_url = 'http://example.com/player';
     testFragment.thumbnail_url = 'http://example.com/thumbnail';
+    const { fragment_id } = await fragmentRepository.save(testFragment);
+
+    const countryRepository = connection.getRepository(Country);
+    const testCountry = new Country();
+    testCountry.name = 'testCountry';
+    const { country_id } = await countryRepository.save(testCountry);
+
+    const bioRepository = connection.getRepository(Bio);
+    const testBio = new Bio();
+    testBio.bio = 'lorem ipsum i tak dalej';
+    testBio.language_code = 'PL';
+    const { bio_id } = await bioRepository.save(testBio);
+
+    const tagRepository = connection.getRepository(Tag);
+    const testTag = new Tag();
+    testTag.name = 'test_tag';
+    const { tag_id } = await tagRepository.save(testTag);
+
+    const personRepository = connection.getRepository(Person);
+    const testPerson = new Person();
+    testPerson.name = 'testName testSurname';
+    const { person_id } = await personRepository.save(testPerson);
+
+    testFragment.person = testPerson;
     await fragmentRepository.save(testFragment);
 
-    // Retrieve and print test data
-    const fragments = await fragmentRepository.find();
+    const dbFragment = await fragmentRepository.findOne({ where: { fragment_id: fragment_id }, relations: ['person'] });
+    console.log(dbFragment);
 
-    console.log(fragments);
-
-    expect(fragments).to.have.lengthOf(1);
-    expect(fragments[0].title).to.equal('Test Fragment');
+    // expect(fragments).to.have.lengthOf(1);
+    expect(dbFragment?.title).to.equal('Test Fragment');
   });
 });
 
