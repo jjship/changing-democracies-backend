bcb7363d6527d1b79f2f3ae92570d2c3f3f46c91
Jacek Skrzypek <devontheroof@gmail.com>
2025-01-04 19:10:53 +0100

    ad ts-node start script

--- cd_backend/src/main.ts
+++ cd_backend/src/main.ts
@@ -3,15 +3,28 @@ import { createBunnyStreamClient } from './services/bunnyStream/bunnyStreamApiCl
 import { ENV } from './env';
 import { syncFragments } from './domain/fragments/syncFragments';
 import { logger } from './services/logger/logger';
+import { setupApp } from './app';
+
+main();
 
 async function main() {
-  const dbConnection = await createDbConnection();
-  const bunnyStream = createBunnyStreamClient({ logger: logger })({
-    baseUrl: ENV.BUNNY_STREAM_BASE_URL,
-    apiKey: ENV.BUNNY_STREAM_API_KEY,
-    libraryId: ENV.BUNNY_STREAM_LIBRARY_ID,
-    collectionId: ENV.BUNNY_STREAM_COLLECTION_ID,
-  });
+  try {
+    const dbConnection = await createDbConnection();
+    const bunnyStream = createBunnyStreamClient({ logger: logger })({
+      baseUrl: ENV.BUNNY_STREAM_BASE_URL,
+      apiKey: ENV.BUNNY_STREAM_API_KEY,
+      libraryId: ENV.BUNNY_STREAM_LIBRARY_ID,
+      collectionId: ENV.BUNNY_STREAM_COLLECTION_ID,
+    });
 
-  await syncFragments({ dbConnection, bunnyStream, logger });
+    await syncFragments({ dbConnection, bunnyStream, logger });
+
+    const app = await setupApp({ dbConnection, bunnyStream });
+
+    await app.listen({ port: ENV.BACKEND_PORT, host: ENV.BACKEND_HOST });
+  } catch (err) {
+    logger.fatal(err);
+
+    process.exit(1);
+  }
 }
