58f745f8b5774427c9f357a7cc6c843a5ea1f2b6
Jacek Skrzypek <devontheroof@gmail.com>
2025-01-25 18:56:34 +0100

    exclude db from cleanup

--- scripts/docker-cleanup.sh
+++ scripts/docker-cleanup.sh
@@ -5,7 +5,7 @@ echo "Starting Docker cleanup..."
 
 docker container prune -f
 docker image prune -a --force --filter "until=24h"
-docker volume prune -f
+docker volume prune -f --filter "label!=keep=true"
 docker network prune -f
 
 echo "Finished Docker cleanup"
