286523ab67b51f3348d27e8bbe8d14dae24c92a9
Jacek Skrzypek <devontheroof@gmail.com>
2025-01-26 17:13:52 +0100

    fix db volume

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,12 @@ set -e
 DB_VOLUME="cd_admin-db_data"
 
 if ! docker volume ls | grep -q $DB_VOLUME; then
-	docker volume create --name $DB_VOLUME
+  docker volume create --name $DB_VOLUME
 fi
+
+docker run --rm \
+  -v $DB_VOLUME:/data \
+  alpine sh -c 'chown -R 1000:1000 /data'
 
 docker compose -f docker-compose.prod.yml down --volumes=false
 
