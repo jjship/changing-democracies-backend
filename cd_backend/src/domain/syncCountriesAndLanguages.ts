3e2ffd7299819f0dd604643ef866a992a6677a8f
Jacek Skrzypek <devontheroof@gmail.com>
2025-01-26 14:49:19 +0100

    local db improvements

--- cd_backend/src/domain/syncCountriesAndLanguages.ts
+++ cd_backend/src/domain/syncCountriesAndLanguages.ts
@@ -1,8 +1,9 @@
 import { FastifyBaseLogger } from 'fastify';
 import { DataSource } from 'typeorm';
-import { CountriesApiClient } from '../services/coutriesApi/countriesApiClient';
+import { CountriesApiClient, CountryData } from '../services/coutriesApi/countriesApiClient';
 import { saveNewCountries } from './countries/saveNewCountries';
 import { saveNewLanguages } from './languages/saveNewLanguages';
+import { ENV } from '../env';
 
 export const syncCountriesAndLanguages = async ({
   dbConnection,
@@ -13,9 +14,36 @@ export const syncCountriesAndLanguages = async ({
   countriesApiClient: CountriesApiClient;
   logger: FastifyBaseLogger;
 }) => {
-  const countriesData = await countriesApiClient.getEuropeanCountries();
+  const countriesData =
+    ENV.NODE_ENV === 'production' ? await countriesApiClient.getEuropeanCountries() : getBaseCountries();
 
   await saveNewLanguages({ dbConnection, logger });
 
   await saveNewCountries({ dbConnection, logger })(countriesData);
 };
+
+function getBaseCountries() {
+  const countriesCodes = [
+    { name: 'Belgium', code: 'BE' },
+    { name: 'Croatia', code: 'HR' },
+    { name: 'Czech Republic', code: 'CZ' },
+    { name: 'Netherlands', code: 'NL' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'Greece', code: 'GR' },
+    { name: 'Lithuania', code: 'LT' },
+    { name: 'Poland', code: 'PL' },
+    { name: 'Portugal', code: 'PT' },
+    { name: 'Romania', code: 'RO' },
+    { name: 'Spain', code: 'ES' },
+  ];
+
+  return countriesCodes.reduce(
+    (acc, country) => {
+      acc.push({ name: country.name, alpha2Code: country.code });
+
+      return acc;
+    },
+    [] as Pick<CountryData, 'name' | 'alpha2Code'>[]
+  );
+}
